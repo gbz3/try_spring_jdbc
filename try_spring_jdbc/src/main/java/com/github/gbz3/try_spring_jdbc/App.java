e04e81c455be6150262c9c44b62ab542ecf264b5
gbz3 <1808483+gbz3@users.noreply.github.com>
2019-08-14 13:45:45 +0900

    init

---
+++ try_spring_jdbc/src/main/java/com/github/gbz3/try_spring_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.github.gbz3.try_spring_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
