c6511ab569ce86e8e06f5936f7d657c9401d4670
gbz3 <1808483+gbz3@users.noreply.github.com>
2019-08-15 12:04:52 +0900

    4.2.6 入力チェック処理の実装

--- try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/app/EchoForm.java
+++ try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/app/EchoForm.java
@@ -2,8 +2,15 @@ package com.github.gbz3.try_spring_webmvc.app;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@SuppressWarnings("deprecation")
 public class EchoForm implements Serializable {
 
+	@NotEmpty
+	@Size(max = 100)
 	private String text;
 
 	public String getText() {
