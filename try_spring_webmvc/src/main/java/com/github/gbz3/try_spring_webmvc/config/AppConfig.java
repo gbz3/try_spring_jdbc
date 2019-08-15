9d3b72930f92260a770ea5553791eb4bb82131b4
gbz3 <1808483+gbz3@users.noreply.github.com>
2019-08-15 11:21:27 +0900

    4.2.2 Spring MVCの適用

---
+++ try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.github.gbz3.try_spring_webmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.github.gbz3.try_spring_webmvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.github.gbz3.try_spring_webmvc.app")
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp();
+	}
+
+}
