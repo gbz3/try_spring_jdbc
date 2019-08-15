74723d61a44f924f3ebcf36cf50f9a6b87b2bc5b
gbz3 <1808483+gbz3@users.noreply.github.com>
2019-08-15 13:05:06 +0900

    5.13 例外ハンドリング

--- try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/app/EchoController.java
+++ try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/app/EchoController.java
@@ -1,11 +1,14 @@
 package com.github.gbz3.try_spring_webmvc.app;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("echo")
@@ -21,9 +24,16 @@ public class EchoController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String echo(@Validated EchoForm form, BindingResult result, Model model) {
 		if(result.hasErrors()) {
-			return "echo/input";
+			//return "echo/input";
+			throw new IllegalStateException("dummy.");
 		}
 		return "echo/output";
+	}
+
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String handleException(Exception e) {
+		return "error";
 	}
 
 }
