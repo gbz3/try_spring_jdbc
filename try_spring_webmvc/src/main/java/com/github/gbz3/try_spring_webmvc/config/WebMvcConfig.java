c922637dc583bcd99f47df6e755b96597abbd625
gbz3 <1808483+gbz3@users.noreply.github.com>
2019-09-14 10:46:40 +0900

    ログ出力追加( AOP )

--- try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/config/WebMvcConfig.java
+++ try_spring_webmvc/src/main/java/com/github/gbz3/try_spring_webmvc/config/WebMvcConfig.java
@@ -2,6 +2,7 @@ package com.github.gbz3.try_spring_webmvc.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,7 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.github.gbz3.try_spring_webmvc.api", "com.github.gbz3.try_spring_webmvc.app"})
+@ComponentScan({"com.github.gbz3.try_spring_webmvc.api", "com.github.gbz3.try_spring_webmvc.app", "com.github.gbz3.try_spring_webmvc.aspect"})
+@EnableAspectJAutoProxy
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	public void configureViewResolvers(ViewResolverRegistry registry) {
